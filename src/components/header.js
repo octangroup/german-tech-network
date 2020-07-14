3dad2fb89765bb838ae12f965c20343b2191871d
karemarsy <karema@octangrp.com>
2020-07-14 07:48:26 +0200

    Create the navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "../styles/app.scss"
 
 const Header = ({ siteTitle }) => (
   <header
@@ -16,16 +17,10 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+      <h1 className="text-red">
+       
           {siteTitle}
-        </Link>
+       
       </h1>
     </div>
   </header>
