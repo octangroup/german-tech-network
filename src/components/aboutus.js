5d9bc43929b65382d20d1a6d662c1a5ccd892f70
karemarsy <karema@octangrp.com>
2020-07-15 15:11:58 +0200

    Fix the font issue

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const AboutUs = ({ siteTitle}) => (
-  <div className="panel z-10 px-0 pt-32 xs:pt-0 sm:pt-12 bg-white t-0 overflow-hidden relative min-h-screen md:h-auto sm:h-auto sm:h-auto">
-    <div class="py-4 w-50 mx-auto text-center">
-		<h1 class="font-primary my-5 text-2xl tracking-wider">{siteTitle}</h1> 
+  <div className="panel z-10 px-0 pt-48 xs:pt-0 sm:pt-12 bg-white t-0 overflow-hidden relative min-h-screen md:h-auto sm:h-auto sm:h-auto">
+    <div class="py-4 w-60 mx-auto text-center">
+		<h1 class="font-primary my-5 text-5xl tracking-wider">{siteTitle}</h1> 
     <div class="h-rem-1 w-rem-18 bg-accent mx-auto rounded-full"></div>
     <div class="w-100 px-2 my-10 tracking-wider leading-loose text-black-50 text-group">	
     <p>
