1e421ff2a8bf2d8ba7bd87f7dd5aa5e0b91092ff
Landry <landos@octangrp.com>
2020-07-20 12:46:32 +0200

    adding the project overview page

---
+++ src/components/cards/project.js
@@ -0,0 +1,33 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import TruncateHtml from "../utilities/TruncateHtml"
+import Translator from "../../utils/translator"
+import { connect } from "react-redux"
+
+class ProjectCard extends React.Component {
+  render() {
+    return (
+      <div className="">
+
+
+<div className="w-40  bg-white">
+        <div className="w-95 h-rem-74 bg-red overflow-hidden">
+
+        <img src={"/img/WeCodeTraining_MurahoTechnologies-1536x1020.jpg"} className="w-100 h-100 overflow-hidden" />
+        </div>
+        <div class="w-65">
+          <h3 class="p-0 m-0">Code+Design Camps</h3>
+          <p class="p-0 m-0">The Code+Design Camp is an educational 
+              methodology that aims...</p>
+        </div>
+
+     </div>
+
+      </div>
+    )
+  }
+}
+
+
+export default Project
---
+++ src/pages/portfolio.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Portfolio = () => (
+  <Layout>
+<div className="w-100 relative py-16">
+          <div className="w-85 mx-auto text-center relative">
+            <h1 class="font-primary text-9xl">Discover our projects</h1>
+            <p class="txet-2xl secondary-font">
+            The German Rwandan Tech Network is a platform that brings together tech-leaders from Rwanda 
+            and Germany.
+            </p>
+
+     </div>
+
+   </div>
+  </Layout>
+)
+
+export default Portfolio
