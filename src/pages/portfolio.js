abeeee63b625719aba013a52edad93feb313e12b
Landry <landos@octangrp.com>
2020-07-22 09:20:21 +0200

    creating the individual page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,14 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
-import ProjectCard from "../cards/project"
-
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-class PortfolioSection extends React.Component {
-  render() {
-    return (
-      <SectionWrapper>
+
+
+const PortfolioSection = () => (
+    <Layout>
         <div class="w-100 relative py-16">
           <div className="w-85 mx-auto text-center relative">
             <h1 class="font-primary text-9xl">Discover our projects</h1>
@@ -18,29 +14,7 @@ class PortfolioSection extends React.Component {
             </p>
 
      </div>
-     <div className="flex flex-wrap justify-center w-95 ml-auto ">
-          {this.props.portfolioSection.map((item, index) => (
-            <ProjectCard key={index} details={item} />
-          ))}
-        </div>
    </div>
-   </SectionWrapper>
-   )
- }
-}
-
-PortfolioSection.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
-  id: PropTypes.string,
-  values: PropTypes.array,
-}
-
-PortfolioSection.defaultProps = {
-  id: null,
-  title: ``,
-  description: ``,
-  values: [],
-}
-
+   </Layout>
+)
 export default PortfolioSection
